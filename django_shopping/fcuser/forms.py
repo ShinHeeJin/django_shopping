a46ef80d6d4543ff9f7d9f94d72d34354652c966
heej <sisesu92@gmail.com>
2020-05-19 00:26:27 +0900

    로그인 기능 추가

--- django_shopping/fcuser/forms.py
+++ django_shopping/fcuser/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.hashers import check_password, make_password
 from .models import Fcuser
 
 class RegisterForm(forms.Form):
@@ -36,6 +37,39 @@ class RegisterForm(forms.Form):
             else:
                 fcuser = Fcuser(
                     email = email,
-                    password = password
+                    password = make_password(password)
                 )
                 fcuser.save()
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages = {
+            'requried' : '이메일을 입력해주세요'
+        },
+        max_length=64, label="이메일"
+    )
+    password = forms.CharField(
+        error_messages = {
+            'required' : '비밀번호를 입력해주세요'
+        },
+        widget=forms.PasswordInput, label="비밀번호"
+    )
+    def clean(self):
+        cleaned_data = super().clean()  # 값 검증
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                fcuser = Fcuser.objects.get(email=email)
+            except Fcuser.DoesNotExist:
+                self.add_error('email', '회원이 존재하지 않습니다.')
+                return # 아래를 체크하지 않는다.
+                
+            # 비밀번호가 틀렸을때
+            if not check_password(password, fcuser.password):
+                self.add_error('password', '비밀번호가 틀렸습니다.')
+
+            # 비밀번호가 맞았을때(성공)
+            else:
+                self.email = fcuser.email
--- django_shopping/fcuser/views.py
+++ django_shopping/fcuser/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from .forms import RegisterForm
+
+from .forms import LoginForm, RegisterForm
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'email':request.session.get('user')})
 
 class RegisterView(FormView):
     template_name = 'register.html'
     form_class = RegisterForm
     success_url = '/' # 성공했을때 구현
 
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+    success_url = '/' # 성공했을때 구현
+
+    def form_valid(self, form): # 유효성 검사가 끝났을때 들어오는 함수(세션저장)
+        self.request.session['user'] = form.email
+        return super().form_valid(form)
